da534fb8799bb08289af829a55302197fd9a2bed
Vish Desai <vishwakarma_d@hotmail.com>
2015-07-20 21:14:41 +0530

    Added profile management as ES6 lazy-loaded Ember Component

--- api_modules/components/profiles/component.js
+++ api_modules/components/profiles/component.js
@@ -1,4 +1,4 @@
-/// <reference path="../../../../typings/node/node.d.ts"/>
+/// <reference path="./../../../typings/node/node.d.ts"/>
 /*
  * Name			: api_modules/components/profiles/component.js
  * Author		: Vish Desai (vishwakarma_d@hotmail.com)
