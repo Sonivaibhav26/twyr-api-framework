1f71f2c8c6b0d7a722ed716675a5864eadedb662
Vish Desai <vishwakarma_d@hotmail.com>
2015-09-10 15:25:19 +0530

    Split profile into profile and login components

---
+++ api_modules/components/login/config.js
@@ -0,0 +1,160 @@
+/*
+ * Name			: api_modules/components/login/config.js
+ * Author		: Vish Desai (vishwakarma_d@hotmail.com)
+ * Version		: 0.6.1
+ * Copyright	: Copyright (c) 2014 Vish Desai (https://www.linkedin.com/in/vishdesai).
+ * License		: The MIT License (http://opensource.org/licenses/MIT).
+ * Description	: The Twy'r API Server Login Component Config
+ *
+ */
+
+"use strict";
+
+exports.development = ({
+	'randomServer': {
+		'protocol': 'https',
+		'options': {
+			'method': 'POST',
+			'host': 'api.random.org',
+			'port': 443,
+			'path': '/json-rpc/1/invoke',
+			'data': {
+				'jsonrpc': '2.0',
+				'method': 'generateStrings',
+				'params': {
+					'apiKey': 'e20ac8ec-9748-4736-a61c-d234ac6ac619',
+					'n': 1,
+					'length': 10,
+					'characters': 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+					'replacement': false
+				},
+				'id': ''
+			}
+		}
+	},
+
+	'notificationServer': {
+		'protocol': 'http',
+
+		'options': {
+			'method': 'POST',
+			'host': 'localhost',
+			'port': 3000
+		},
+
+		'resetPasswordPath': '/mailer/resetPassword',
+		'newAccountPath': '/mailer/newAccount'
+	}
+});
+
+exports.test = ({
+	'randomServer': {
+		'protocol': 'https',
+		'options': {
+			'method': 'POST',
+			'host': 'api.random.org',
+			'port': 443,
+			'path': '/json-rpc/1/invoke',
+			'data': {
+				'jsonrpc': '2.0',
+				'method': 'generateStrings',
+				'params': {
+					'apiKey': 'e20ac8ec-9748-4736-a61c-d234ac6ac619',
+					'n': 1,
+					'length': 10,
+					'characters': 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+					'replacement': false
+				},
+				'id': ''
+			}
+		}
+	},
+
+	'notificationServer': {
+		'protocol': 'http',
+
+		'options': {
+			'method': 'POST',
+			'host': 'localhost',
+			'port': 3000
+		},
+
+		'resetPasswordPath': '/mailer/resetPassword',
+		'newAccountPath': '/mailer/newAccount'
+	}
+});
+
+exports.stage = ({
+	'randomServer': {
+		'protocol': 'https',
+		'options': {
+			'method': 'POST',
+			'host': 'api.random.org',
+			'port': 443,
+			'path': '/json-rpc/1/invoke',
+			'data': {
+				'jsonrpc': '2.0',
+				'method': 'generateStrings',
+				'params': {
+					'apiKey': 'e20ac8ec-9748-4736-a61c-d234ac6ac619',
+					'n': 1,
+					'length': 10,
+					'characters': 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+					'replacement': false
+				},
+				'id': ''
+			}
+		}
+	},
+
+	'notificationServer': {
+		'protocol': 'http',
+
+		'options': {
+			'method': 'POST',
+			'host': 'localhost',
+			'port': 3000
+		},
+
+		'resetPasswordPath': '/mailer/resetPassword',
+		'newAccountPath': '/mailer/newAccount'
+	}
+});
+
+exports.production = ({
+	'randomServer': {
+		'protocol': 'https',
+		'options': {
+			'method': 'POST',
+			'host': 'api.random.org',
+			'port': 443,
+			'path': '/json-rpc/1/invoke',
+			'data': {
+				'jsonrpc': '2.0',
+				'method': 'generateStrings',
+				'params': {
+					'apiKey': 'e20ac8ec-9748-4736-a61c-d234ac6ac619',
+					'n': 1,
+					'length': 10,
+					'characters': 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+					'replacement': false
+				},
+				'id': ''
+			}
+		}
+	},
+
+	'notificationServer': {
+		'protocol': 'http',
+
+		'options': {
+			'method': 'POST',
+			'host': 'localhost',
+			'port': 3000
+		},
+
+		'resetPasswordPath': '/mailer/resetPassword',
+		'newAccountPath': '/mailer/newAccount'
+	}
+});
+
